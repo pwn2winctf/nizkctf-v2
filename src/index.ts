63e0473238e2b6812c10dd1e3d494f3eecead8f0
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-06-13 12:17:11 -0300

    Fix port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import firebaseInstance from './firebase'
 const database = firebaseInstance
 
 const run = async () => {
-  app({ port: parseInt(process.env.PORT) || 8080, database })
+  app({ port: process.env.PORT ? parseInt(process.env.PORT) : 8080, database })
 }
 
 run()
