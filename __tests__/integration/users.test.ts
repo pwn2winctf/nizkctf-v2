8aad3293447294fc0771341afa1074d115a28b8c
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-06-19 23:01:59 -0300

    Add new test

--- __tests__/integration/users.test.ts
+++ __tests__/integration/users.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest'
 import app from '../../src/app'
-import { prepareDatabase, DatabaseStructure } from '../../utils'
+import { prepareDatabase, DatabaseStructure, APIError } from '../../utils'
 
 const store: DatabaseStructure = {
   teams: {},
@@ -38,6 +38,28 @@ describe('Users endpoints', () => {
     expect(status).toBe(201)
   })
 
+  it('Should not create a already exists user account', async () => {
+    const database = prepareDatabase(store)
+
+    const data = {
+      email: 'nizkctf@mailinator.com',
+      displayName: 'User tester',
+      password: '123456'
+    }
+
+    await database.users.register(data)
+
+    const { body, status } = await request(app({ database }))
+      .post('/users')
+      .send(data)
+
+    const firstError: APIError = body.errors[0]
+
+    expect(status).toBe(400)
+    expect(firstError.code).toEqual('semantic')
+    expect(firstError.message).toEqual('Already exists a user with this email')
+  })
+
   it('Should not create a user account without email', async () => {
     const database = prepareDatabase(store)
 
@@ -51,9 +73,9 @@ describe('Users endpoints', () => {
       .post('/users')
       .send(data)
 
-    const firstError: string = body.errors[0].param
+    const firstError: APIError = body.errors[0]
 
-    expect(firstError).toEqual('email')
+    expect(firstError.param).toEqual('email')
     expect(status).toBe(422)
   })
 
@@ -84,5 +106,89 @@ describe('Users endpoints', () => {
     expect(refreshToken.length).toBeGreaterThan(0)
 
     expect(status).toBe(200)
+  })
+
+  it('Should not login without email', async () => {
+    const database = prepareDatabase(store)
+
+    const data = {
+      email: 'nizkctf@mailinator.com',
+      displayName: 'User tester',
+      password: '123456'
+    }
+
+    await database.users.register(data)
+
+    const { body, status } = await request(app({ database }))
+      .post('/users/login')
+      .send({ ...data, email: undefined })
+
+    const firstError: APIError = body.errors[0]
+
+    expect(firstError.param).toEqual('email')
+    expect(status).toBe(422)
+  })
+
+  it('Should not login without password', async () => {
+    const database = prepareDatabase(store)
+
+    const data = {
+      email: 'nizkctf@mailinator.com',
+      displayName: 'User tester',
+      password: '123456'
+    }
+
+    await database.users.register(data)
+
+    const { body, status } = await request(app({ database }))
+      .post('/users/login')
+      .send({ ...data, password: undefined })
+
+    const firstError: APIError = body.errors[0]
+
+    expect(firstError.param).toEqual('password')
+    expect(status).toBe(422)
+  })
+
+  it('Should not login without exists user', async () => {
+    const database = prepareDatabase(store)
+
+    const data = {
+      email: 'nizkctf@mailinator.com',
+      displayName: 'User tester',
+      password: '123456'
+    }
+
+    const { body, status } = await request(app({ database }))
+      .post('/users/login')
+      .send(data)
+
+    const firstError: APIError = body.errors[0]
+
+    expect(status).toBe(400)
+    expect(firstError.code).toBe('semantic')
+    expect(firstError.message).toBe('Not exists a user with this email')
+  })
+
+  it('Should not login with invalid password', async () => {
+    const database = prepareDatabase(store)
+
+    const data = {
+      email: 'nizkctf@mailinator.com',
+      displayName: 'User tester',
+      password: '123456'
+    }
+
+    await database.users.register(data)
+
+    const { body, status } = await request(app({ database }))
+      .post('/users/login')
+      .send({ ...data, password: '654321' })
+
+    const firstError: APIError = body.errors[0]
+
+    expect(status).toBe(400)
+    expect(firstError.code).toBe('semantic')
+    expect(firstError.message).toBe('Wrong password')
   })
 })
