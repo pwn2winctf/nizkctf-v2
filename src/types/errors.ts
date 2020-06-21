1413b38152686ed93876b6a5720f6f09a50d9fcd
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-06-21 00:57:17 -0300

    Create custom errors

---
+++ src/types/errors.ts
@@ -0,0 +1,42 @@
+import { Result, ValidationError as ValidationErrorArgs } from 'express-validator'
+
+class HttpError extends Error {
+  public statusCode:number
+  constructor (statusCode:number, message:string) {
+    super(message)
+    this.statusCode = statusCode
+    Object.setPrototypeOf(this, ValidationError.prototype)
+  }
+}
+
+export class ValidationError extends HttpError {
+  public errors:ValidationErrorArgs[]
+
+  constructor (args:Result<ValidationErrorArgs>) {
+    super(422, 'Validation error')
+    this.errors = args.array()
+    Object.setPrototypeOf(this, ValidationError.prototype)
+  }
+}
+
+export class AuthorizationError extends HttpError {
+  constructor (message:string, statusCode = 403) {
+    super(statusCode, message)
+
+    Object.setPrototypeOf(this, AuthorizationError.prototype)
+  }
+}
+
+export class SemanticError extends HttpError {
+  constructor (message: string, statusCode = 422) {
+    super(statusCode, message)
+    Object.setPrototypeOf(this, SemanticError.prototype)
+  }
+}
+
+export class NotFoundError extends HttpError {
+  constructor (message: string, statusCode = 404) {
+    super(statusCode, message)
+    Object.setPrototypeOf(this, NotFoundError.prototype)
+  }
+}
