fa40c49b4cf7276cea1036a7efef94fae930ccf6
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-02-21 20:28:39 -0300

    Add challenges routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { Database } from '../app'
 import teamsRouter from './teams.route'
 import usersRouter from './users.route'
 import scoreRouter from './score.route'
+import challengesRouter from './challenges.route'
 
 export default function (database: Database): Router {
   const router = Router()
@@ -12,6 +13,7 @@ export default function (database: Database): Router {
   router.use('/teams', teamsRouter(database))
   router.use('/users', usersRouter(database))
   router.use('/score', scoreRouter(database))
+  router.use('/challenges', challengesRouter(database))
 
   router.get('/', async (_, res) => {
     res.status(200).send('Good Luck')
