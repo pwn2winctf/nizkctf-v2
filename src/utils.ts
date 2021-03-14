a625786afa5c062c1d584ac54a7da468cbb65ef4
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-03-14 17:11:10 -0300

    Creates function to get user data from jwt and a fuction to create sha256

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,8 @@
+import { createHash } from 'crypto'
+import jwtDecode from 'jwt-decode'
+
 import { dynamic_scoring as dynamicScore } from '../constants.json'
+import { MissingTokenError } from './types/errors.type'
 
 export function computeScore (numberOfSolves: number): number {
   const { K, V, minpts, maxpts } = dynamicScore
@@ -9,4 +13,19 @@ export function computeScore (numberOfSolves: number): number {
       Math.floor(maxpts - K * Math.log2((numberOfSolves + V) / (1 + V)))
     )
   )
+}
+
+export function createSHA256 (data: string): string {
+  return createHash('sha256').update(data).digest('hex')
+}
+
+export async function getUserDataFromJWT (token: string): Promise<{ uid: string, email: string, verified: boolean, displayName: string }> {
+  try {
+    // eslint-disable-next-line
+    const { user_id: uid, email, display_name: displayName, verified } = jwtDecode(token) as { user_id: string, email: string, verified: boolean, display_name: string }
+
+    return { uid, email, displayName, verified }
+  } catch (err) {
+    throw new MissingTokenError('Invalid token')
+  }
 }
