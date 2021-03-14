7c9cce1e6d95fcdd48955105ddcb1340334cb5ea
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-03-14 20:47:15 -0300

    Remove challenges route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,6 @@ import { Database } from '../app'
 import teamsRouter from './teams.route'
 import usersRouter from './users.route'
 import scoreRouter from './score.route'
-import challengesRouter from './challenges.route'
 
 export default function (database: Database): Router {
   const router = Router()
@@ -13,7 +12,6 @@ export default function (database: Database): Router {
   router.use('/teams', teamsRouter(database))
   router.use('/users', usersRouter(database))
   router.use('/score', scoreRouter(database))
-  router.use('/challenges', challengesRouter(database))
 
   router.get('/', async (_, res) => {
     res.status(200).send('Good Luck')
