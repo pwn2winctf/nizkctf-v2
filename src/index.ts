1c776a6dc3f23ba799b865a72427f961fd3b90f8
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-03-14 20:46:50 -0300

    Fix FIREBASE CREDENTIALS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import app from './app'
-import { DATABASE_CREDENTIALS, DATABASE_URL, PORT } from './config'
-import { init as initDabase } from './firebase'
+import { FIREBASE_CREDENTIALS, DATABASE_URL, PORT } from './config'
+import { init as initFirebaseDabase } from './databases/firebase'
 
-const databaseCredentials = DATABASE_CREDENTIALS
+const databaseCredentials = FIREBASE_CREDENTIALS
 const databaseURL = DATABASE_URL
 
 async function run () {
-  const database = initDabase({ credential: databaseCredentials, databaseURL })
+  const database = await initFirebaseDabase({ credential: databaseCredentials, databaseURL })
   app({ port: PORT ? parseInt(PORT) : 8080, database })
 }
 
