f51bb97d27e55fdfd323ebb32e73bacaa4bb8f28
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-03-14 20:52:48 -0300

    Add tests to validate token

--- __tests__/integration/solves.test.ts
+++ __tests__/integration/solves.test.ts
@@ -4,9 +4,11 @@ import {
   prepareDatabase,
   DatabaseStructure,
   claimFlag,
-  APIError
+  APIError,
+  createJWTToken
 } from '../../utils'
 import app from '../../src/app'
+import { getUserDataFromJWT } from '../../src/utils'
 
 const initialState: DatabaseStructure = {
   teams: {},
@@ -89,6 +91,35 @@ describe('Teams solves', () => {
       .send(data)
 
     expect(status).toBe(200)
+  })
+
+  it('Should not accept submission without a valid token', async () => {
+    const database = prepareDatabase(store)
+
+    const challenge = await database.challenges.get(challengeId)
+    const proof = await claimFlag(team.name, 'CTF-BR{123}', challenge)
+
+    const data = {
+      proof,
+      challengeId
+    }
+
+    const { uid, email, displayName } = await getUserDataFromJWT(token)
+
+    const wrongPrivateKey = '-----BEGIN RSA PRIVATE KEY-----\nMIICXAIBAAKBgQC+uQGmgtlFkJ330HiV7p8aRv0gvL22i0H683/udK9BVNXbNad7\nYKuepUUFldF9nWmcNncEz/Mhe6B+rGeyYLLY67QPEJjHDpoTbGNqnKdZRI+sLKdl\nbdNWM73RzhSYRNil8h7EuKzCbWF8D61faUZtXX/t7xYsVql0iOy3OpYuWQIDAQAB\nAoGBAL2carKjVeTeMN6FyMyurkjOmKtqWWXZLuCSIvbr1Kyr7e0Ae6CibBzODD/Q\nQbSTwcJHN652Gy6HnPky5+kkT8i3pWKeq336l24lsLy3xlNUjbOFi/ixu2/+LH4X\nE8WA6K6KRgw41rG46/3gTL9F0grak08KVBI4fJwfxxQLuAPtAkEA4RtFd9EAt5qb\nhfuSSp/2R6o2rXzCNzFNAti/iTfiL1tBREFvMt1bVGpWrmzAAiRYTV21NKIb9GQ3\nygi0ncNSKwJBANjlt9NGuJ9/9mWMLv6DC+xw8AE+vOMjpFs2kUtXfjyxjdwytBSn\nE4T2KR2FMeKSeqq7JLQDXhxkaIapHyV+M4sCQBw04puE4OYgxWQghdA8Awwv0/Ih\nM+TelJvKtRVonzr34FHBMUHaYttNB9eLYPPjwfzPxSj3NzGEbUkvlhI+q0kCQH2Q\nX18cbf1IYyheg0YsL1bt+sN9wnPsSlXTvUFUoWwZjfqQD9h08MPkjeDdHwf/rKvy\nM0lTOcRODIUIm1NlQAkCQEvzLUARO5tKUHwt87kUISugtKo2+HDe+5DSjUW0m4Ih\nlo1uYRoOjqR0QZSv2wqFnH7fiJzYsvpQicIm+ZwOQg0=\n-----END RSA PRIVATE KEY-----'
+
+    const invalidToken = createJWTToken({ userId: uid, email, displayName: displayName, verified: true }, wrongPrivateKey)
+
+    const { status, body } = await request(app({ database }))
+      .post(`/teams/${team.id}/solves`)
+      .set({ Authorization: invalidToken })
+      .set('content-type', 'application/json')
+      .send(data)
+
+    expect(status).toBe(403)
+    const firstError: APIError = body.errors[0]
+    expect(firstError.code).toBe('authorization')
+    expect(firstError.message).toBe('Invalid token')
   })
 
   it('Should not accept submission without challengeId', async () => {
--- __tests__/integration/teams.test.ts
+++ __tests__/integration/teams.test.ts
@@ -1,7 +1,8 @@
 import request from 'supertest'
 
-import { prepareDatabase, DatabaseStructure, APIError } from '../../utils'
+import { prepareDatabase, DatabaseStructure, APIError, createJWTToken } from '../../utils'
 import app from '../../src/app'
+import { getUserDataFromJWT } from '../../src/utils'
 
 const store: DatabaseStructure = {
   teams: {},
@@ -60,6 +61,31 @@ describe('Teams endpoints', () => {
       .send(data)
 
     expect(response.status).toBe(201)
+  })
+
+  it('Should not create team with a invalid token', async () => {
+    const database = prepareDatabase(store)
+
+    const data = {
+      name: 'Team test',
+      countries: ['br', 'us', 'jp', 'zw', 'pt']
+    }
+
+    const { uid, email, displayName } = await getUserDataFromJWT(token)
+
+    const wrongPrivateKey = '-----BEGIN RSA PRIVATE KEY-----\nMIICXAIBAAKBgQC+uQGmgtlFkJ330HiV7p8aRv0gvL22i0H683/udK9BVNXbNad7\nYKuepUUFldF9nWmcNncEz/Mhe6B+rGeyYLLY67QPEJjHDpoTbGNqnKdZRI+sLKdl\nbdNWM73RzhSYRNil8h7EuKzCbWF8D61faUZtXX/t7xYsVql0iOy3OpYuWQIDAQAB\nAoGBAL2carKjVeTeMN6FyMyurkjOmKtqWWXZLuCSIvbr1Kyr7e0Ae6CibBzODD/Q\nQbSTwcJHN652Gy6HnPky5+kkT8i3pWKeq336l24lsLy3xlNUjbOFi/ixu2/+LH4X\nE8WA6K6KRgw41rG46/3gTL9F0grak08KVBI4fJwfxxQLuAPtAkEA4RtFd9EAt5qb\nhfuSSp/2R6o2rXzCNzFNAti/iTfiL1tBREFvMt1bVGpWrmzAAiRYTV21NKIb9GQ3\nygi0ncNSKwJBANjlt9NGuJ9/9mWMLv6DC+xw8AE+vOMjpFs2kUtXfjyxjdwytBSn\nE4T2KR2FMeKSeqq7JLQDXhxkaIapHyV+M4sCQBw04puE4OYgxWQghdA8Awwv0/Ih\nM+TelJvKtRVonzr34FHBMUHaYttNB9eLYPPjwfzPxSj3NzGEbUkvlhI+q0kCQH2Q\nX18cbf1IYyheg0YsL1bt+sN9wnPsSlXTvUFUoWwZjfqQD9h08MPkjeDdHwf/rKvy\nM0lTOcRODIUIm1NlQAkCQEvzLUARO5tKUHwt87kUISugtKo2+HDe+5DSjUW0m4Ih\nlo1uYRoOjqR0QZSv2wqFnH7fiJzYsvpQicIm+ZwOQg0=\n-----END RSA PRIVATE KEY-----'
+
+    const invalidToken = createJWTToken({ userId: uid, email, displayName: displayName, verified: true }, wrongPrivateKey)
+
+    const { body, status } = await request(app({ database }))
+      .post('/teams')
+      .set({ Authorization: invalidToken })
+      .send(data)
+
+    expect(status).toBe(403)
+    const firstError: APIError = body.errors[0]
+    expect(firstError.code).toBe('authorization')
+    expect(firstError.message).toBe('Invalid token')
   })
 
   it('Should not create already exists team', async () => {
