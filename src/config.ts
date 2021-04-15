8c358e152f9004cca40df1ebf6600611624effa9
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-04-15 00:21:46 -0300

    Fix type

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import dotEnvFlow from 'dotenv-flow'
 
 dotEnvFlow.config()
 
-export const APP_ENV = process.env.APP_ENV || 'development'
+export const APP_ENV: 'development' | 'production' | 'test' = process.env.APP_ENV === 'production' ? 'production' : process.env.APP_ENV === 'test' ? 'test' : 'development'
 export const RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET || ''
 export const RECAPTCHA_REQUIRED = process.env.RECAPTCHA_REQUIRED === 'true'
 export const FIREBASE_CREDENTIALS = JSON.parse(process.env.CREDS || '')
