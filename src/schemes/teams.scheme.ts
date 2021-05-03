7cf364d40e6f85f7d4bcb53eedb9deb112f8c27a
Paulo Matias <matias@ufscar.br>
2021-05-03 11:13:25 -0300

    Improve error message on countries validation

--- src/schemes/teams.scheme.ts
+++ src/schemes/teams.scheme.ts
@@ -8,9 +8,11 @@ export const newTeamScheme:ValidationChain[] = [
     .custom((name: string) => name.length > 0),
   check('countries')
     .isArray({ max: constants.maxCountriesFlag, min: 0 })
+    .withMessage(`must have max ${constants.maxCountriesFlag} country flags`)
     .custom((countries: string[]) =>
       countries.every((item: string) => constants.countries.includes(item))
     )
+    .withMessage('country list contains an unknown country')
 ]
 
 export const newSolveScheme:ValidationChain[] = [check('challengeId').isString(), check('proof').isString().isBase64()]
