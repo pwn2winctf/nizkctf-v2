ee1e302779aeb70f027edb97aeb68dfb575a0c75
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-05-02 21:29:11 -0300

    Fix code style

--- src/cache.ts
+++ src/cache.ts
@@ -1,14 +1,14 @@
 import { Request } from 'express'
 import mcache from 'memory-cache'
 
-export const getCache = (req: Request) => {
+export const getCache = (req: Request): any => {
   const key = '__express__' + req.originalUrl || req.url
   const cachedData = mcache.get(key)
 
   return cachedData
 }
 
-export const updateCache = (duration: number, data: any, req: Request) => {
+export const updateCache = (duration: number, data: any, req: Request): void => {
   const key = '__express__' + req.originalUrl || req.url
   mcache.put(key, data, duration * 1000)
 }
